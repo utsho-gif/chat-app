546115338131ad8c4fb05b6935dee3977848c234
KT-STL-Utsho <you@example.com>
2024-10-03 18:13:10 +0600

    avatar page

--- app/[site]/components/AuthForm.tsx
+++ app/[site]/components/AuthForm.tsx
@@ -2,7 +2,7 @@
 
 import { signIn, useSession } from 'next-auth/react';
 import axios from 'axios';
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import { BsGithub, BsGoogle } from 'react-icons/bs';
 import { useRouter } from 'next/navigation';
@@ -16,16 +16,16 @@ import toast from 'react-hot-toast';
 type Variant = 'LOGIN' | 'REGISTER';
 
 const AuthForm = () => {
-  //   const session = useSession();
+  const session = useSession();
   const router = useRouter();
   const [variant, setVariant] = useState<Variant>('LOGIN');
   const [isLoading, setIsLoading] = useState(false);
 
-  //   useEffect(() => {
-  //     if (session?.status === 'authenticated') {
-  //       router.push('/conversations')
-  //     }
-  //   }, [session?.status, router]);
+  useEffect(() => {
+    if (session?.status === 'authenticated') {
+      router.push('/conversations');
+    }
+  }, [session?.status, router]);
 
   const toggleVariant: any = useCallback(() => {
     if (variant === 'LOGIN') {
